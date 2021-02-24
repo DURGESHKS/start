f84651f2687c86cac393e4762468428b06a987e6
DURGESH KUMAR <77738041+DURGESHKS@users.noreply.github.com>
2021-02-25 04:25:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+//////////active 24/7///////////
+let count = 0;
 
+setInterval(
+
+  () =>
+
+    require("node-fetch")(process.env.URL).then(() =>
+
+      console.log(`[${++count}] here i pinged ${process.env.URL}`)
+
+    ),
+
+  300000
+
+);
